4290d3c1dd8416e490ef7ad9f9a73685b8c9d95b
Tom Moyer <tommoyer@gmail.com>
2025-02-23 12:12:33 -0500

    Added a very basic webhook reciever

---
+++ webhook/webhook.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)  # Standard Flask app
+
+
+@app.route("/")        # Standard Flask endpoint
+def hello_world():
+    return "Hello, World!"
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook(data):
+    data = request.json
+    print("Received webhook data:", data)
+    return jsonify({'message': 'Webhook received successfully'}), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
